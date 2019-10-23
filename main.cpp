16e2115a4e399d9ff1efca4cf94dad5e111571d3
Adrian Czech <a.czech@stud.elka.pw.edu.pl>
2019-10-23 18:20:09 +0200

    Jakoś działa

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+template <typename T>
+class orderIterator :public std::vector<T>::iterator
+{
+//public: //DEBUG
+    typedef typename std::vector<T>::iterator iterator;
+
+    iterator current;
+    std::vector<T> owner;
+public:
+    T& operator*()
+    {
+        return *current;
+    }
+    explicit orderIterator(std::vector<T> vec)
+    {
+        owner = vec;
+        current = owner.begin();
+        for(auto i = owner.begin(); i!= owner.end(); ++i)
+        {
+            if(*i<*current) current = i;
+        }
+    }//CONSTRUCTOR
+    orderIterator& operator++()
+    {
+        for(auto i = current+1; i!=owner.end(); ++i)
+            if(*i == *current && i!=current)
+            {
+                current = i;
+                return *this;
+            }
+
+        auto newCurrent = owner.begin();
+        while(newCurrent != owner.end() && *newCurrent <= *current)
+            newCurrent++;
+        if(newCurrent == owner.end() )
+        {
+            current = owner.end();
+            return *this;
+        }
+
+        for(auto i = owner.begin(); i!=owner.end(); ++i)
+        {
+            if(*i>*current && *i<*newCurrent)
+                newCurrent = i;
+        }
+        current = newCurrent;
+        return *this;
+    }
+    bool end()
+    {
+        return current == owner.end();
+    }
+
+};
+
+int main()
+{
+    std::vector <int> myVec{1,4,8,8};
+
+    orderIterator<int> oi(myVec);
+    while( !oi.end())
+    {
+        cout << *oi << " ";
+        ++oi;
+    }
+}
