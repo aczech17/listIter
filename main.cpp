76e654e409829d1d4a34e02e0abcab48fc9486c1
Adrian Czech <a.czech@stud.elka.pw.edu.pl>
2019-10-23 18:45:44 +0200

    użyłem wskaźników

--- main.cpp
+++ main.cpp
@@ -11,40 +11,40 @@ class orderIterator :public std::vector<T>::iterator
     typedef typename std::vector<T>::iterator iterator;
 
     iterator current;
-    std::vector<T> owner;
+    std::vector<T>* owner;
 public:
     T& operator*()
     {
         return *current;
     }
-    explicit orderIterator(std::vector<T> vec)
+    explicit orderIterator(std::vector<T>& vec)
     {
-        owner = vec;
-        current = owner.begin();
-        for(auto i = owner.begin(); i!= owner.end(); ++i)
+        owner = &vec;
+        current = owner->begin();
+        for(auto i = owner->begin(); i!= owner->end(); ++i)
         {
             if(*i<*current) current = i;
         }
     }//CONSTRUCTOR
     orderIterator& operator++()
     {
-        for(auto i = current+1; i!=owner.end(); ++i)
+        for(auto i = current+1; i!=owner->end(); ++i)
             if(*i == *current && i!=current)
             {
                 current = i;
                 return *this;
             }
 
-        auto newCurrent = owner.begin();
-        while(newCurrent != owner.end() && *newCurrent <= *current)
+        auto newCurrent = owner->begin();
+        while(newCurrent != owner->end() && *newCurrent <= *current)
             newCurrent++;
-        if(newCurrent == owner.end() )
+        if(newCurrent == owner->end() )
         {
-            current = owner.end();
+            current = owner->end();
             return *this;
         }
 
-        for(auto i = owner.begin(); i!=owner.end(); ++i)
+        for(auto i = owner->begin(); i!=owner->end(); ++i)
         {
             if(*i>*current && *i<*newCurrent)
                 newCurrent = i;
@@ -52,11 +52,15 @@ public:
         current = newCurrent;
         return *this;
     }
-    bool end()
-    {
-        return current == owner.end();
-    }
 
+    bool operator==(const iterator i) const
+    {
+        return current == i;
+    }
+    bool operator!=(const iterator i) const
+    {
+        return current != i;
+    }
 };
 
 int main()
@@ -64,7 +68,7 @@ int main()
     std::vector <int> myVec{1,4,8,8};
 
     orderIterator<int> oi(myVec);
-    while( !oi.end())
+    while( oi != myVec.end() )
     {
         cout << *oi << " ";
         ++oi;
